32ded889fdd4b5a89ceff92cb52df2a76fbf5d41
Kafilat <kafilatadewumi61@gmail.com>
2023-02-22 22:29:01 +0100

    update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -15,23 +15,25 @@ const Navbar = () => {
   window.addEventListener("scroll", changeNavbarColor);
 
   return (
-    <nav className={colorChange ? 'navbar-scroll' : 'navbar colorChange' }>
-      <div className="nav-logo">
-        <Link to="/">
-          <img src="/logo.svg" alt="" />
-        </Link>
-      </div>
-      <div className="nav-items">
-        <Link to="/purpose">Purpose</Link>
-        <Link to="/community">Community</Link>
-        <Link to="/careers">
-          Careers <sup>(11)</sup>
-        </Link>
-        <Link to="/learn">Learn</Link>
-        <button className="nav-button">
-          <img src="/location.svg" alt="" />
-          <span>United Kingdom</span>
-        </button>
+    <nav className={colorChange ? "navbar-scroll" : "colorChange"}>
+      <div className="inner-nav">
+        <div className="nav-logo">
+          <Link to="/">
+            <img src="/logo.svg" alt="" />
+          </Link>
+        </div>
+        <div className="nav-items">
+          <Link to="/purpose">Purpose</Link>
+          <Link to="/community">Community</Link>
+          <Link to="/careers">
+            Careers <sup>(11)</sup>
+          </Link>
+          <Link to="/learn">Learn</Link>
+          <button className="nav-button">
+            <img src="/location.svg" alt="" />
+            <span>United Kingdom</span>
+          </button>
+        </div>
       </div>
     </nav>
   );
