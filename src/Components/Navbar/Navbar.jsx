9d3d889018c5e6955bf3315d0ba21cf47c476950
Kafilat <kafilatadewumi61@gmail.com>
2023-02-23 01:24:38 +0100

    update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import "./Navbar.css"
 
 const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
   const [colorChange, setColorchange] = useState(false);
   const changeNavbarColor = () => {
     if (window.scrollY >= 80) {
@@ -33,6 +34,31 @@ const Navbar = () => {
             <img src="/location.svg" alt="" />
             <span>United Kingdom</span>
           </button>
+        </div>
+        {/* Mobile Tab */}
+        <div>
+          <div
+            className=" lg:hidden"
+            onClick={() => setShowNavbar(!showNavbar)}
+          >
+            <img className="w-8 h-8" src="/menu.svg" alt="" />
+          </div>
+          <div
+            className={`absolute grid ${
+              showNavbar ? "right-4" : "-right-[10rem]"
+            } h-60 w-24 top-20 transition-all duration-150`}
+          >
+            <Link to="/purpose">Purpose</Link>
+            <Link to="/community">Community</Link>
+            <Link to="/careers">
+              Careers <sup>(11)</sup>
+            </Link>
+            <Link to="/learn">Learn</Link>
+            <button className="nav-button">
+              <img src="/location.svg" alt="" />
+              <span>United Kingdom</span>
+            </button>
+          </div>
         </div>
       </div>
     </nav>
